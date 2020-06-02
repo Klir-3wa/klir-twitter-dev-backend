01f58ba39ee3ce5598a60b7cf4092c8625fc56f6
lahcen001 <lahcen0000lh@gmail.com>
2020-06-02 16:57:55 +0200

    back end laravel

---
+++ app/Http/Controllers/TController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+use  App\Twitter;
+use Illuminate\Http\Request;
+
+class TController extends Controller
+{
+   public function create(Request $request){
+   $twitter =new Twitter();
+
+   $twitter->twitterID = $request->input('twitterID');
+
+   $twitter->save();
+   return response()->json($twitter);
+   }
+
+
+
+   public function get(){
+    return response()->json(Twitter::get());
+}
+
+}
---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/twitter','TController@create');
+Route::get('/id','TController@get');
